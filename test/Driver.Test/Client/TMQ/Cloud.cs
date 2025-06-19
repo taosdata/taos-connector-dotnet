da7280569ec2288cbb1ffffd3054a4b0cccbc0b5
t_max <1172915550@qq.com>
2025-06-19 15:10:58 +0800

    enh: improve cloud connection validation in CloudConsumerTest

--- test/Driver.Test/Client/TMQ/Cloud.cs
+++ test/Driver.Test/Client/TMQ/Cloud.cs
@@ -12,7 +12,7 @@ namespace Driver.Test.Client.TMQ
         {
             var db = "cs_test";
             var topic = "cs_tmq_test_topic";
-            if (string.IsNullOrEmpty(this._cloudConnectString))
+            if (string.IsNullOrEmpty(this._cloudConnectString) || this._cloudTMQCfg == null)
             {
                 _output.WriteLine("Cloud connection string is not set. Skipping CloudConsumerTest.");
                 return;
