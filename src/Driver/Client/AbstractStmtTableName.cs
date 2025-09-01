6e17593d295d7361c6cb32ac3b11e79f3099977c
t_max <1172915550@qq.com>
2025-09-01 14:08:24 +0800

    fix: add table name to builder when table info is not found

--- src/Driver/Client/AbstractStmtTableName.cs
+++ src/Driver/Client/AbstractStmtTableName.cs
@@ -21,12 +21,12 @@ namespace TDengine.Driver.Client
                     throw new ArgumentException("Table name cannot be null or empty");
                 }
 
-                _tableNameBuilder.Add(tableName);
                 if (_tableInfos.TryGetValue(tableName, out var info))
                 {
                 }
                 else
                 {
+                    _tableNameBuilder.Add(tableName);
                     info = new Stmt2BindTableInfo
                     {
                         TableName = tableName
